5a8aa366412b4a27c3a8f187aa3250d3fdb9da09
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-11-24 11:59:12 -0300

    trabalhando corrupção no código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Luiz", "Araujo","12345678910", "luiz@luiz.com");
+        student.AddSubscription(subscription);
+    }
+}
