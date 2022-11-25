694aa3862c33d7b6858cc8aa874b44265bb260d1
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-11-25 17:59:21 -0300

    feature: alterando variável

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -7,7 +7,6 @@ namespace PaymentContext.Tests.Entities;
 [TestClass]
 public class StudentTests
 {
-    private readonly Name _name;
     private readonly Email _email;
     private readonly Document _document;
     private readonly Address _address;
@@ -15,11 +14,11 @@ public class StudentTests
 
     public StudentTests()
     {
-        _name = new Name("Bruce", "Wayne");
+        var name = new Name("Bruce", "Wayne");
         _document = new Document("35111507795", EDocumentType.CPF);
         _email = new Email("batman@dc.com");
         _address = new Address("Rua 1", "1234", "Bairro Legal", "Gotham", "SP", "BR", "13400000");
-        _student = new Student(_name, _document, _email);            
+        _student = new Student(name, _document, _email);            
 
     }
 
