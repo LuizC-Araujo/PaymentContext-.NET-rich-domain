378031e51026ceffd0c1bd461246217131176eba
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-01 13:52:51 -0300

    feature: refacting code for reuse fake class

--- PaymentContext.Tests/Handlers/CreateBoletoSubscriptionHandlerTests.cs
+++ PaymentContext.Tests/Handlers/CreateBoletoSubscriptionHandlerTests.cs
@@ -8,36 +8,44 @@ namespace PaymentContext.Tests.Handlers;
 [TestClass]
 public class CreateBoletoSubscriptionHandlerTests
 {
+    private readonly CreateBoletoSubscriptionCommand _command;
+    private readonly CreateBoletoSubscriptionHandler _handler;
+
+    public CreateBoletoSubscriptionHandlerTests()
+    {
+        _handler = new CreateBoletoSubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
+        _command = new CreateBoletoSubscriptionCommand();
+        
+        _command.FirstName = "Peter";
+        _command.LastName = "Parker";
+        _command.Document = "00000000000";
+        _command.Email = "peterparker@spider.com.br";
+        _command.BarCode = "652185418417";
+        _command.BoletoNumber = "541588478";
+        _command.PaymentNumber = "489259";
+        _command.PaidDate = DateTime.Now;
+        _command.ExpireDate = DateTime.Now.AddMonths(1);
+        _command.Total = 60;
+        _command.TotalPaid = 60;
+        _command.Payer = "Spider Corp";
+        _command.PayerDocument = "8888888888";
+        _command.PayerDocumentType = EDocumentType.CPF;
+        _command.PayerEmail = "spider@spider.com.br";
+        _command.Street = "assas";
+        _command.Number = "588";
+        _command.Neighborhood = "asfds";
+        _command.City = "adasd";
+        _command.State = "sa";
+        _command.Country = "sa";
+        _command.ZipCode = "25454889";
+    }
+    
     [TestMethod]
     public void ShouldReturnErrorWhenDocumentExists()
-    {
-        var handler = new CreateBoletoSubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
-        var command = new CreateBoletoSubscriptionCommand();
+    { 
+        _command.Document = "99999999999";
         
-    command.FirstName = "Peter"; 
-    command.LastName = "Parker"; 
-    command.Document = "99999999999"; 
-    command.Email = "peterparker@spider.com.br"; 
-    command.BarCode = "652185418417"; 
-    command.BoletoNumber = "541588478"; 
-    command.PaymentNumber = "489259"; 
-    command.PaidDate = DateTime.Now; 
-    command.ExpireDate = DateTime.Now.AddMonths(1); 
-    command.Total = 60; 
-    command.TotalPaid = 60; 
-    command.Payer = "Spider Corp"; 
-    command.PayerDocument = "8888888888"; 
-    command.PayerDocumentType = EDocumentType.CPF; 
-    command.PayerEmail = "spider@spider.com.br"; 
-    command.Street = "assas"; 
-    command.Number = "588"; 
-    command.Neighborhood = "asfds"; 
-    command.City = "adasd"; 
-    command.State = "sa"; 
-    command.Country = "sa"; 
-    command.ZipCode = "25454889";
-
-    handler.Handle(command);
-    Assert.AreEqual(false, handler.IsValid);
+        _handler.Handle(_command);
+        Assert.AreEqual(false, _handler.IsValid);
     }
 }
